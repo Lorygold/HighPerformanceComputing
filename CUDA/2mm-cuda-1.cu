b3bd1d72cd8588bc336e504995e959935b5d732a
adam taoufiq <33988878+adamtao14@users.noreply.github.com>
2023-12-14 18:28:40 +0100

    Update 2mm-cuda-1.cu

--- CUDA/2mm-cuda-1.cu
+++ CUDA/2mm-cuda-1.cu
@@ -87,35 +87,31 @@ __global__ void kernelCUDA1(double* tmp, double* A, double* B, double alpha, int
     for (int k = 0; k < nk; k++){
       // Accumulate product
       sum += alpha * A[(i * nk) + k] * B[(k * nj) + j];
-      printf("%d\n",B[(nj*k)+j]);
     }
     // Write result
     tmp[(i * nj) + j] = sum;
   }
 }
 
-
-/* Main computational kernel. The whole function will be timed,
-   including the call and return. */
-static void kernel_2mm(int ni, int nj, int nk, int nl,
-                       DATA_TYPE alpha,
-                       DATA_TYPE beta,
-                       DATA_TYPE POLYBENCH_2D(tmp, NI, NJ, ni, nj),
-                       DATA_TYPE POLYBENCH_2D(A, NI, NK, ni, nk),
-                       DATA_TYPE POLYBENCH_2D(B, NK, NJ, nk, nj),
-                       DATA_TYPE POLYBENCH_2D(C, NL, NJ, nl, nj),
-                       DATA_TYPE POLYBENCH_2D(D, NI, NL, ni, nl))
+__global__ void kernelCUDA2(double* tmp, double* D, double* C, double beta, int ni, int nj, int nl)
 {
-    //int i, j, k;
-  
-    // for (i = 0; i < _PB_NI; i++)
-    //   for (j = 0; j < _PB_NL; j++)
-    //   {
-    //     D[i][j] *= beta;
-    //     for (k = 0; k < _PB_NJ; ++k)
-    //       D[i][j] += tmp[i][k] * C[k][j];
-    //   }
+
+  // Get row and column indices
+  int i = blockIdx.y * blockDim.y + threadIdx.y;
+  int j = blockIdx.x * blockDim.x + threadIdx.x;
+  // Check bounds
+  if (i < ni && j < nj) {
+    
+   double sum = 0.0; 
+   for (int k = 0; k < nl; k++){
+      // Accumulate product
+      sum += tmp[(i * nl) + k] * C[(k * nl) + j];
+    }
+    // Write result
+    D[(i * nl) + j] = sum*beta;
+  }
 }
+
 
 int main(int argc, char **argv)
 {
@@ -135,50 +131,25 @@ int main(int argc, char **argv)
   double* D = new double[ni*nl];
   double* tmp = new double[ni*nj];
 
-  /*
-  POLYBENCH_2D_ARRAY_DECL(tmp, DATA_TYPE, NI, NJ, ni, nj);
-  POLYBENCH_2D_ARRAY_DECL(A, DATA_TYPE, NI, NK, ni, nk);
-  POLYBENCH_2D_ARRAY_DECL(B, DATA_TYPE, NK, NJ, nk, nj);
-  POLYBENCH_2D_ARRAY_DECL(C, DATA_TYPE, NL, NJ, nl, nj);
-  POLYBENCH_2D_ARRAY_DECL(D, DATA_TYPE, NI, NL, ni, nl);
-  */
   /* Initialize array(s). */
   init_array(ni, nj, nk, nl, &alpha, &beta,A,B,C,D);
-  
-  /*
-  for(int z=0; z< ni*nk; z++){
-    if(z%5 == 0)
-      printf("\n");
-    printf("%f ", C[z]);
-  }
-  */
+
   /* Start timer. */
   polybench_start_instruments;
 
-  /* Run kernel. */
-  /*
-  kernel_2mm(ni, nj, nk, nl,
-             alpha, beta,
-             POLYBENCH_ARRAY(tmp),
-             POLYBENCH_ARRAY(A),
-             POLYBENCH_ARRAY(B),
-             POLYBENCH_ARRAY(C),
-             POLYBENCH_ARRAY(D));
-  */
   // Allocate device memory
   double *d_a, *d_b, *d_c, *d_d, *d_tmp;
-  printf("%d\n", ni*nk);
   gpuErrchk(cudaMalloc((void **)&d_a, sizeof(double) * ni * nk));
   gpuErrchk(cudaMalloc((void **)&d_b, sizeof(double) * nk * nj));
-  //cudaMalloc((void **)&d_c, sizeof(DATA_TYPE) * NL * nj);
-  //cudaMalloc((void **)&d_d, sizeof(DATA_TYPE) * ni * NL);
+  gpuErrchk(cudaMalloc((void **)&d_c, sizeof(double) * nl * nj));
+  gpuErrchk(cudaMalloc((void **)&d_d, sizeof(double) * ni * nl));
   gpuErrchk(cudaMalloc((void **)&d_tmp, sizeof(double) * ni * nj));
   //printf("N=%d, BLOCK_SIZE=%d grid_size=%d\n",N,BLOCK_SIZE,((N+(BLOCK_SIZE-1))/BLOCK_SIZE));
   // Data copy from host to device
   gpuErrchk(cudaMemcpy(d_a, A, sizeof(double) * ni * nk, cudaMemcpyHostToDevice));
   gpuErrchk(cudaMemcpy(d_b, B, sizeof(double) * nk * nj, cudaMemcpyHostToDevice));
-  //cudaMemcpy(d_c, C, sizeof(DATA_TYPE) * NL * nj, cudaMemcpyHostToDevice);
-  //cudaMemcpy(d_d, D, sizeof(DATA_TYPE) * ni * NL, cudaMemcpyHostToDevice);
+  gpuErrchk(cudaMemcpy(d_c, C, sizeof(double) * nl * nj, cudaMemcpyHostToDevice));
+  gpuErrchk(cudaMemcpy(d_d, D, sizeof(double) * ni * nl, cudaMemcpyHostToDevice));
 
   dim3 block_size(BLOCK_SIZE,BLOCK_SIZE);
   dim3 grid_size((N+BLOCK_SIZE-1) / (BLOCK_SIZE),(N+BLOCK_SIZE-1) / (BLOCK_SIZE));
@@ -187,6 +158,7 @@ int main(int argc, char **argv)
   kernelCUDA1<<<grid_size,block_size>>>(d_tmp, d_a, d_b, alpha, ni, nj, nk);
   gpuErrchk( cudaPeekAtLastError() );
   gpuErrchk( cudaDeviceSynchronize() );
+  kernelCUDA2<<<grid_size,block_size>>>(d_tmp, d_d, d_c, beta, ni, nj, nl);
   gpuErrchk(cudaMemcpy(tmp, d_tmp, sizeof(double) * ni * nj, cudaMemcpyDeviceToHost));
   for(int z = 0; z < ni*nj; z++){
     printf("value=%f\n",tmp[z]);
@@ -195,9 +167,6 @@ int main(int argc, char **argv)
   polybench_stop_instruments;
   polybench_print_instruments;
 
-  /* Prevent dead-code elimination. All live-out data must be printed
-     by the function call in argument. */
-  //polybench_prevent_dce(print_array(ni, nl, POLYBENCH_ARRAY(D)));
 
   /* Be clean. */
   delete[] tmp;
